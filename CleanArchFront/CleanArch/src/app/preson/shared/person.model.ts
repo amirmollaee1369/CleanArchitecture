dfd04c7c6fae85ee9407a30608d0583ee7002189
Amir Molaei <amir.mollaee1369@gmail.com>
2022-03-11 23:26:58 +0330

    delete angular app for recreate

--- CleanArchFront/CleanArch/src/app/preson/shared/person.model.ts
+++
@@ -1,9 +0,0 @@
-export class PersonModel{
-    public Id! :number;
-    public FirstName!: string;
-    public LastName!: string;
-    public Email!: string;
-    public DateofBirth!: string;
-    public PhoneNumber!: string;
-    public RegDate!: string;
-}
--- CleanArchFront/CleanArch/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: 'https://localhost:44314/api/',
-  baseUrlHost: 'https://localhost:44314/'
-};
